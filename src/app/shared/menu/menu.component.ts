1b8bea7842b53b8db44de2725ecc20d4f05c23f3
Victor <v.g.m_@hotmail.com>
2021-03-23 21:26:26 +0100

    Sobre mi realizado y empezando portafolio

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-menu',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+
+  menu: MenuItem[] = [
+    { ruta: '/inicio', texto: 'Inicio'},
+    { ruta: '/sobremi', texto: 'Sobre mí'},
+    { ruta: '/portafolio1', texto: 'Portafolio'},
+    { ruta: '/curriculum', texto: 'Curriculum'},
+    { ruta: '/contacto', texto: 'Contacto'}
+  ];
 
   constructor() { }
 
